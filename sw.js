1054a44b437612cc21a96eefc431f5ea26dde8fc
jiahanbirdy04-gif <jiahanbirdy04@gmail.com>
2026-07-20 11:44:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+var CACHE_NAME = "kdrama-archive-v1";
+var FILES_TO_CACHE = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (key) { return key !== CACHE_NAME; })
+          .map(function (key) { return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
